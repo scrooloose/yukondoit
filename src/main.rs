1b09a009078e460ee1271414d7ebc299912b4d16
Richard Wall <wallrj@users.noreply.github.com>
2017-03-15 16:37:39 +0000

    Show available moves

--- src/main.rs
+++ src/main.rs
@@ -9,46 +9,125 @@ use rand::{thread_rng, Rng};
 use itertools::join;
 use colored::*;
 
+#[derive(Debug)]
 struct Suit {
     name: &'static str,
+    color: &'static str,
 }
 
-static SUITS: [Suit; 4] = [Suit { name: "CLUB" },
-                           Suit { name: "DIAMOND" },
-                           Suit { name: "HEART" },
-                           Suit { name: "SPADE" }];
+impl PartialEq for Suit {
+    fn eq(&self, other: &Suit) -> bool {
+        self.name == other.name && self.color == other.color
+    }
+}
 
+static SUITS: [Suit; 4] = [Suit {
+                               name: "CLUB",
+                               color: "BLACK",
+                           },
+                           Suit {
+                               name: "DIAMOND",
+                               color: "RED",
+                           },
+                           Suit {
+                               name: "HEART",
+                               color: "RED",
+                           },
+                           Suit {
+                               name: "SPADE",
+                               color: "BLACK",
+                           }];
+
+#[derive(Debug)]
 struct Rank {
     name: &'static str,
+    value: u8,
 }
 
-static RANKS: [Rank; 13] = [Rank { name: "ACE" },
-                            Rank { name: "TWO" },
-                            Rank { name: "THREE" },
-                            Rank { name: "FOUR" },
-                            Rank { name: "FIVE" },
-                            Rank { name: "SIX" },
-                            Rank { name: "SEVEN" },
-                            Rank { name: "EIGHT" },
-                            Rank { name: "NINE" },
-                            Rank { name: "TEN" },
-                            Rank { name: "JACK" },
-                            Rank { name: "QUEEN" },
-                            Rank { name: "KING" }];
+impl PartialEq for Rank {
+    fn eq(&self, other: &Rank) -> bool {
+        self.name == other.name && self.value == other.value
+    }
+}
 
+static RANKS: [Rank; 13] = [Rank {
+                                name: "ACE",
+                                value: 1,
+                            },
+                            Rank {
+                                name: "TWO",
+                                value: 2,
+                            },
+                            Rank {
+                                name: "THREE",
+                                value: 3,
+                            },
+                            Rank {
+                                name: "FOUR",
+                                value: 4,
+                            },
+                            Rank {
+                                name: "FIVE",
+                                value: 5,
+                            },
+                            Rank {
+                                name: "SIX",
+                                value: 6,
+                            },
+                            Rank {
+                                name: "SEVEN",
+                                value: 7,
+                            },
+                            Rank {
+                                name: "EIGHT",
+                                value: 8,
+                            },
+                            Rank {
+                                name: "NINE",
+                                value: 9,
+                            },
+                            Rank {
+                                name: "TEN",
+                                value: 10,
+                            },
+                            Rank {
+                                name: "JACK",
+                                value: 11,
+                            },
+                            Rank {
+                                name: "QUEEN",
+                                value: 12,
+                            },
+                            Rank {
+                                name: "KING",
+                                value: 13,
+                            }];
+
+#[derive(Debug)]
 struct Card {
     suit: &'static Suit,
     rank: &'static Rank,
 }
 
+impl PartialEq for Card {
+    fn eq(&self, other: &Card) -> bool {
+        self.suit == other.suit && self.rank == other.rank
+    }
+}
+
 impl Card {
-    fn to_char(&self) -> Option<char> {
+    fn to_string(&self) -> ColoredString {
         let unicode_name = format!(
             "PLAYING CARD {} OF {}S",
             self.rank.name,
             self.suit.name,
         );
-        return unicode_names::character(&unicode_name);
+        let character = unicode_names::character(&unicode_name).unwrap().to_string();
+        if self.suit.color == "RED" {
+            return character.red();
+        } else {
+            return character.white();
+        }
     }
 }
 
@@ -126,11 +205,7 @@ fn draw(table: &Table) {
             let mut card_char = match column_card_iterators[column_index].next() {
                 Some(card) => {
                     card_found = true;
-                    let card_string = card.to_char().unwrap().to_string();
-                    match card.suit.name {
-                        "DIAMOND" | "HEART" => card_string.red(),
-                        _ => card_string.white(),
-                    }
+                    card.to_string()
                 }
                 None => "-".to_string().white(),
             };
@@ -187,10 +262,40 @@ fn read_coordinate(message: &str) -> Coordinate {
     return Coordinate { x: x, y: y };
 }
 
+fn next_cards<'a, 'b>(table: &'a Table, last_card: &'b Card) -> Vec<&'a Card> {
+    let mut next = vec![];
+    for column in table.columns.iter() {
+        if column.cards.contains(&last_card) {
+            continue;
+        }
+        for card in column.cards.iter().skip(column.hidden_index) {
+            if card.rank.value != last_card.rank.value - 1 {
+                continue;
+            }
+            if card.suit.color == last_card.suit.color {
+                continue;
+            }
+            next.push(*card);
+        }
+    }
+    return next;
+}
+
+fn show_moves(table: &Table) {
+    for column in table.columns.iter() {
+        let last_card = column.cards.last().unwrap();
+        for card in next_cards(&table, last_card) {
+            print!("{} / {}\t", card.to_string(), last_card.to_string());
+        }
+    }
+    println!("");
+}
+
 fn main() {
     let deck = Deck::new().shuffle();
     (0..).fold(deal(&deck), |t, _| {
         draw(&t);
+        show_moves(&t);
         let source = read_coordinate("Enter a source coordinate.");
         let destination = read_coordinate("Enter a destination coordinate.");
         return t.move_card(source, destination);
